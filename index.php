efc3f37e3b0d65b631097478abecbff665a970e8
GLeoni17 <gabriel.lduarte@outlook.com>
2020-09-25 21:14:11 -0300

    Salvando em html

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
         <?php include "func.php"; ?>
         <script>
             function aparece(id, h){
-                console.log(id);
+
                     if(h==2){
                         var l=prompt("Insira o novo horario");
                     }else{
                         var l=prompt("Insira a nova atividade");
                     }
-                if(l[2]!=":" && h==2){ 
+
+                if((l[2]!=":" && h==2) || (l.length > 5 && h==2)){ 
                     alert("Insira no formato 00:00");
+                }else if(l[0]=="2" && l[1]=="4"){
+                    alert("Insira um horário entre 00:00 a 23:00");
                 }else{
                     $("#"+id).html("<span>"+l+"</span>");
                 }
@@ -23,23 +26,30 @@
             }
             $(document).ready(function(){
                 $("#salvar").click(function(){
-                    var conteudo = "<table class=\"table table-responsive\">"+$("#tabela").html()+"</table>";
+                    var conteudo = "<link rel=\"stylesheet\" href=\"style.css\">";
+                    conteudo+="<link href=\"bootstrap-4.4.1-dist/css/bootstrap.min.css\" type=\"text/css\" rel=\"stylesheet\" />";
+                    conteudo+=$("#table").html();
                     var d = new Date();
-                    $.post("salvar.php", {"conteudo":conteudo, "data_y":d.getFullYear(), "data_m":d.getMonth()+1, "data_d":d.getDay()}, function(msg){
-                        console.log(msg)
+                    $.post("salvar.php", {"conteudo":conteudo, "data_y":d.getFullYear(), "data_m":d.getMonth()+1, "data_d":d.getDate()}, function(msg){
+                        
                     });
                 });
             });
         </script>
     </head>
     <body>
-        <div class="row justify-content-center ">
-            <div class="col-auto">
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+            <div class="col-12" id="table" style="overflow:hidden;">
                 <table id="tabela" class="table table-responsive">
                     <?php table();?>
                 </table>
             </div>
+            <div class="row">
+                <span class="col-md-5"></span>
+                <button id="salvar" type="button" class="btn btn-lg btn-dark btn-block outra-cor col-md-1">Salvar</button>
+                <span class="col-md-5"></span>
+            </div>
         </div>
-        <button id="salvar" type="button">Salvar</button>
+        
     </body>
 </html>
--- salvar.php
+++ salvar.php
@@ -1,11 +1,12 @@
 <?php
-    require_once 'vendor/autoload.php';
+    /*require_once 'vendor/autoload.php';
     use Dompdf\Dompdf;
     $dompdf = new Dompdf();
     $tabela = $_POST["conteudo"];
     $dompdf->loadhtml($tabela);
     $dompdf->setPaper('A4', 'portrait');
     $dompdf->render();
-    $dompdf->stream();
-    file_put_contents('Tabela.pdf'.$_POST["data_y"].$_POST["data_m"].$_POST["data_d"], $dompdf->output());
+    $dompdf->stream();*/
+    $nome_arquivo = "Tabela".$_POST["data_y"].$_POST["data_m"].$_POST["data_d"];
+    file_put_contents($nome_arquivo.".html", $_POST["conteudo"]);
 ?>
